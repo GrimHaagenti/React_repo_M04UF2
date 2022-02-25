91fe641ec99b3fd43cb442b9746bcc846f3ac806
enti <enti@enti.enti>
2022-02-25 14:28:50 +0100

    ejemplo de react

---
+++ ejemplo_react/src/Titulito.js
@@ -0,0 +1,17 @@
+import Piesito from './Piesito'
+
+function Titulito (){
+
+	return (
+		<div id="titulo">
+		<h1> OLA K ASE</h1>
+		<p><strong><em> ASDOJADOJA</em></strong></p>
+		
+		<Piesito />
+		</div>
+	);
+
+}
+
+
+export default Titulito;
